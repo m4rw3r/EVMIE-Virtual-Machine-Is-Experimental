b185dca607e18126869d7905e2cbfb8383be9b71
m4rw3r <m4rw3r@gmail.com>
2012-05-27 11:04:19 +0200

    Added file and line number to fatal errors

--- src/Error.h
+++ src/Error.h
@@ -3,7 +3,10 @@
 
 #include <stdio.h>
 
+/* TODO: Proper error functions which unwind the stack? */
+
 #define Error_fatalError(...) \
+fprintf(stderr, "ERROR (%s:%d): ", __FILE__, __LINE__); \
 fprintf(stderr, __VA_ARGS__);exit(-1);
 
 #endif
